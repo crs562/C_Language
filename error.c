6790733326549a6959a5d327002b060ebfa36bc2
Chaitanya Shah <acs.chaitanya35@gmail.com>
2017-12-25 16:50:05 -0500

    Create error.c

---
+++ error.c
@@ -0,0 +1,10 @@
+/*
+ ============================================================================
+ Name        : error.c
+ Author      : Chaitanya Shah
+ Version     : v1
+ Copyright   : 
+ Description : Correct the error of the 
+ Style       : ANSI
+ ============================================================================
+ */
