14c80e63ac1f72033605cd69c5359f0f9938bf09
Chaitanya Shah <acs.chaitanya35@gmail.com>
2017-12-29 23:23:13 -0500

    Update factorial.c

--- factorial.c
+++ factorial.c
@@ -10,12 +10,16 @@
  */
 
 #include <stdio.h>
+
 main()
 {
 	int N, i, mul = 1;
+
 	printf("Enter a number N: ");
 	scanf("%d", &N);
+
 	for(i = 1; i <= N; i++)
 		mul = mul * i;
+
 	printf("Factorial of %d is %d.\n", N, mul);
 }
