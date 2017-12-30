1365beac271b2b175199f4af9878717c7072aaae
Chaitanya Shah <acs.chaitanya35@gmail.com>
2017-12-29 19:39:18 -0500

    Update factorial.c

--- factorial.c
+++ factorial.c
@@ -1,1 +1,21 @@
+/*
+ ==================================================================================================================================
+ Name        : Nadd.c
+ Author      : Chaitanya Shah
+ Version     : v1
+ Copyright   : 
+ Description : Calculating 1 + 2 + 3 + ... + N.
+ Style	     : ANSI-style
+ ==================================================================================================================================
+ */
 
+#include <stdio.h>
+main()
+{
+	int N, i, mul = 1;
+	printf("Enter a number N: ");
+	scanf("%d", &N);
+	for(i = 1; i <= N; i++)
+		mul = mul * i;
+	printf("Factorial of %d is %d.\n", N, mul);
+}
